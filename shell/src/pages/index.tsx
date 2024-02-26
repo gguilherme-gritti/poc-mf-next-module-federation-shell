3c2717e28a983f4bfdde5a7ff4867f636aadb15b
Guilherme Gritti <goulart.gritti@outlook.com>
2024-02-26 11:24:27 -0300

    change to pages directory and setup module federation (with warnings)

---
+++ shell/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import dynamic from "next/dynamic";
+
+const Header = dynamic(
+  () => import("header/component/Header").then((m) => m.Header),
+  {
+    ssr: false,
+  }
+);
+
+export default function Home() {
+  return (
+    <>
+      <h1>Micro-frontends Polirepo Next JS </h1>
+      <Header />
+    </>
+  );
+}
